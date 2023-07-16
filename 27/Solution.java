57758ce4bcf0d0576707299e66cf3cc513ddcb68
chenlinbo <993443108@qq.com>
2023-07-16 21:09:28 +0800

    add 27

---
+++ 27/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int idx = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[idx] = nums[i];
+                idx++;
+                count++;
+            }
+        }
+        return count;
+    }
+}
