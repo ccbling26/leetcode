77bb3122eb475e93056c6577bc7a48c60bed6066
chenlinbo <993443108@qq.com>
2023-08-11 15:19:23 +0800

    add 1572

--- 1572/solution.go
+++ 1572/solution.go
@@ -4,10 +4,10 @@ func diagonalSum(mat [][]int) int {
 	n := len(mat)
 	res := 0
 	for i := 0; i < n; i++ {
-		res += mat[i][i] + mat[i][n - i - 1]
+		res += mat[i][i] + mat[i][n-i-1]
 	}
-	if n % 2 == 1 {
-		res -= mat[n / 2][n / 2]
+	if n%2 == 1 {
+		res -= mat[n/2][n/2]
 	}
 	return res
 }
