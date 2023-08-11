e9834c0e88549c6b2de175ace00e820168168b9c
chenlinbo <993443108@qq.com>
2023-08-11 14:16:39 +0800

    add 1572

---
+++ 1572/solution.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        res = 0
+        n = len(mat)
+        for i in range(n):
+            res += mat[i][i] + mat[i][n - i - 1]
+        if n % 2:
+            res -= mat[n // 2][n // 2]
+        return res
