206b32628389807575149026280415a360d847a0
chenlinbo <993443108@qq.com>
2023-10-01 00:18:57 +0800

    add 2136

--- 2136/solution.py
+++ 2136/solution.py
@@ -3,4 +3,11 @@ from typing import List
 
 class Solution:
     def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
-        
+        arr = list(zip(plantTime, growTime))
+        arr.sort(key=lambda x: -x[1])
+        res = 0
+        cur = 0
+        for p, g in arr:
+            cur += p
+            res = max(res, cur + g + 1)
+        return res - 1
