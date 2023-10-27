d837dcad8ad207ff01fdf854209f8c6f4ac9def9
chenlinbo <993443108@qq.com>
2023-10-27 16:52:02 +0800

    add 1465

---
+++ 1465/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+        int x = Math.max(horizontalCuts[0], h - horizontalCuts[horizontalCuts.length - 1]);
+        int y = Math.max(verticalCuts[0], w - verticalCuts[verticalCuts.length - 1]);
+        for (int i = 1; i < horizontalCuts.length; i++) {
+            x = Math.max(x, horizontalCuts[i] - horizontalCuts[i - 1]);
+        }
+        for (int i = 1; i < verticalCuts.length; i++) {
+            y = Math.max(y, verticalCuts[i] - verticalCuts[i - 1]);
+        }
+        return (int) ((long) x * y % 1000000007);
+    }
+}
