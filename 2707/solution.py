715fd6d9708026956c3f1b7dd48a8c4a46106c9b
chenlinbo <993443108@qq.com>
2024-01-09 15:39:56 +0800

    add 2707

--- 2707/solution.py
+++ 2707/solution.py
@@ -4,18 +4,19 @@ from typing import List
 class Trie:
     def __init__(self):
         self.children = [None for _ in range(26)]
-        self.isEnd = True
-    
+        self.isEnd = False
+
     def insert(self, word: str):
         node = self
         for c in word:
             k = ord(c) - ord("a")
-            if self.children[k] is None:
-                self.children[k] = Trie()
-            node = self.children[k]
+            if node.children[k] is None:
+                node.children[k] = Trie()
+            node = node.children[k]
         node.isEnd = True
 
-    def track(self, node: "Trie", k: int):
+    def track(self, node: "Trie", c: str):
+        k = ord(c) - ord("a")
         if node is None or node.children[k] is None:
             return None, False
         node = node.children[k]
@@ -25,12 +26,11 @@ class Trie:
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
         n = len(s)
-        d = [n + 1] * (n + 1)
-        d[0] = 0
+        d = [0] + [n + 1] * n
         trie = Trie()
-        for e in dictionary:
-            trie.insert(reversed(e))
-        for i in range (1, n + 1):
+        for word in dictionary:
+            trie.insert(reversed(word))
+        for i in range(1, n + 1):
             d[i] = d[i - 1] + 1
             node = trie
             for j in range(i - 1, -1, -1):
